4fc22801a7bb01aa26e8e6a6f173ab55e443c76e
JambonoLeJambon <60428074+JambonoLeJambon@users.noreply.github.com>
2020-05-27 12:25:00 +0200

    modif en php et includes
    fuck sebastien les potos

---
+++ Visite.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php include_once('includes/header.html');
+          include_once('includes/navbar.html')?>
+        <!--Milieu de la page contenant deux parties-->
+        <div class="row">
+            <!--Partie gauche du milieu de la page-->
+            <div class="main">
+                <h1>Organiser sa visite</h1>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d168125.76201210462!2d1.853839449199484!3d48.82072863645609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67d94d7b14c75%3A0x538fcc15f59ce8f!2sCh%C3%A2teau%20de%20Versailles!5e0!3m2!1sfr!2sfr!4v1578430389274!5m2!1sfr!2sfr"
+				width="800" height="600" frameborder="0" style="border:0;" allowfullscreen="" align = "right"></iframe>
+				<p>
+				Informations Générales
+				<br>
+				<h1>Visites:</h1>
+				</p>
+				<p>
+				<br>
+					> Durée moyenne de la visite:
+				<br>
+					90 minutes
+				<br>
+				<br>
+				<h1>Type de patrimoine culturel:</h1>
+				</p>
+				<p>
+				<br>
+					> Site et monuments historiques
+				<br>
+				<br>
+				<h1>Patrimoine historique:</h1>
+				</p>
+				<p>
+				<br>
+					> Château
+				<br>
+					> Jardins
+				<br>
+				   > Palais des glaces
+				<br>
+				   > Parc
+				<br>
+				   > Ecuries
+				<br>
+				<br>
+				<h1>Période:</h1>
+				</p>
+				<p>
+				<br>
+					> XVIIe siècle
+				<br>
+					> XVIIIe siècle
+				<br>
+					> XIXe siècle
+				</p>
+                <img src=" "/>
+            </div>
+            <!--Fin de la partie gauche du milieu de la page-->
+        </div>
+        <!--Fin du milieu de la page-->
+        <?php include_once('includes/footer.html')?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html>
+  <body>
+  <?php include_once('includes/header.html');
+        include_once('includes/navbar.html')?>
+
+        <!--Milieu de la page contenant deux parties-->
+        <div class="row">
+            <!--Partie gauche du milieu de la page-->
+            <div class="main">
+                <h1 style="text-align: center">Bienvenue à Versailles</h1>
+                <!--Slider présentation du chateau-->
+                    <p>
+						<img class = "chateau" src="Images/Chateau.jpg"
+						alt="Château" height = "400" width = "600"
+						style = "margin:0 auto;"/>
+                    </p> <hr/>
+                    <!--Fin du slider présentation du chateau-->
+                <h1>Présentation</h1>
+				<p>Le château de Versailles vu du ciel</p>
+				<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/9cGohlT3UAc?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<p>Source: Château de Versailles</p>
+            </div>
+            <!--Fin de la partie gauche du milieu de la page-->
+            <!--Partie gauche du milieu de la page-->
+            <div class="side">
+            </div>
+            <!--Fin de la partie droite du milieu de la page-->
+        </div>
+        <!--Fin du milieu de la page-->
+        <?php include_once('includes/footer.html') ?>
+    </body>
+</html>

dda926b3fbe8ca123c5c4736b51f7c7b99608c0d
Uniffay <56744682+Uniffay@users.noreply.github.com>
2020-06-06 18:22:19 +0200

    frise eng remis

--- histoire.php
+++ histoire.php
@@ -4,8 +4,15 @@
   <body>
     <?php include_once('includes/navbar.php')?>
     <div class="main">
-      <h1>Frise chronologique retraçant l'histoire du Château de Versailles</h1>
-      <img class = "frise" src="Images/frise.png" alt="Frise" usemap="#image-map"/>
+
+      <h1><?php echo frise_histoire;?></h1>
+       <?php
+          if($_GET["lang"] === "fr"){
+              echo "<img class = \"frise\" src=\"Images/frise.png\" alt=\"Frise\"/>";
+          }
+          else{
+              echo "<img class = \"frise\" src=\"Images/frise_eng.png\" alt=\"Frise\"/>";
+          }?>
       <map name="image-map">
           <area target="_blank" alt="LouisXIII" title="LouisXIII" href="LouisXIII" coords="311,43,459,145" shape="rect">
           <area target="_blank" alt="LouisXIV" title="LouisXIV" href="LouisXIV" coords="311,145,459,335" shape="rect">

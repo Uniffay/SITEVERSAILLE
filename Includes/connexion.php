cf8079ab8dee0d53f5da54b7b9a1e78a9178e7e1
JambonoLeJambon <60428074+JambonoLeJambon@users.noreply.github.com>
2020-06-06 18:33:47 +0200

    alaide

--- Includes/connexion.php
+++ Includes/connexion.php
@@ -1,14 +1,15 @@
 <?PHP
-/* Connexion au serveur et à la base de données */
-$host="sqletud.u-pem.fr";
-$user="vdomin01"; /* Votre login */
-$pwd="xc7yo2ieJs"; /* Votre mot de passe */
-$db="vdomin01_db"; /* Le nom de votre base : de la forme ici : xxx_db avec xxx votre login */
-    // Connexion avec avec PDO
+/* Connexion au serveur et à la base de données */
+$host="localhost";
+$user="root"; /* Votre login */
+$pwd=""; /* Votre mot de passe */
+$db="tp7"; /* Le nom de votre base : de la forme ici : xxx_db avec xxx votre login */
+// Connexion avec avec PDO
 try{
 $con='mysql:host='.$host.';dbname='.$db;
-$dbh = new PDO($con,$user,$pwd,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)); }
+$dbh = new PDO($con,$user,$pwd,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+}
 catch(Exception $e){
-die('Connexion impossible à la base de données !'.$e->getMessage());
+die('Connexion impossible à la base de données !'.$e->getMessage());
 }
 ?>
--- login.php
+++ login.php
@@ -2,37 +2,6 @@
 session_start();
  ?>
 
- <?php
-   if (isset($_POST['submit']) && $_POST['submit'] == 'LOGIN') {
-     include_once('includes/connexion.php');
-     $login = $_POST['login'];
-     $mdp = $_POST['mdp'];
-     echo $login;
-     echo $mdp;
-
-
-
-     $results=$dbh->query("SELECT login,password FROM user WHERE login = '$login' AND password = '$mdp'");
-
-     $ligne = $results->fetch();
-     $login_valide = $ligne['login'];
-     $pwd_valide = $ligne['password'];
-
-     if (!(empty($login)) && !(empty($mdp))) {
-
-       if ($login_valide == $login && $pwd_valide == $mdp) {
-
-         $_SESSION['login'] = $login;
-         $_SESSION['pwd'] = $mdp;
-         header('Location: index.php');
-       }
-       else {
-         echo "La connexion a échoué. Nom d'utilisateur ou mot de passe incorrect.";
-       }
-     }
-   }
- ?>
-
 <html>
   <head>
      <meta charset="utf-8">
@@ -46,15 +15,22 @@ session_start();
         <div id="container">
             <!-- zone de connexion -->
 
-            <form action="" method="POST">
+            <form action="verification.php" method="POST">
                 <h1>Connexion</h1>
                 <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="login"/>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
                 <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="mdp"/>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                <input type="submit" id="submit" name="submit" value='LOGIN'/>
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
             </form>
         </div>
         <?php include_once('includes/footer.php') ?>

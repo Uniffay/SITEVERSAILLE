5c6cc36a8b7547d27d0fb1df7e7e5cbb1adea969
Alan GARCIA <a.garcia.g007@gmail.com>
2020-06-06 16:42:49 +0200

    Added "Billeterie"

---
+++ Includes/billeterie.php
@@ -0,0 +1,7 @@
+<?php
+
+  echo "<h1> Vous êtes intéressé par Versailles ?</h1>";
+  echo "<h3>Acheter votre billet dès maintenant ";
+  echo '<a href="http://www.chateauversailles.fr/preparer-ma-visite/billets-tarifs" target="_blank">ici</a> !</h3>';
+
+?>

9be775f52c7c378fcfdc3edb19b236f77eb5c7c6
IssouLeDingo <60429644+IssouLeDingo@users.noreply.github.com>
2020-06-06 18:49:40 +0200

    Register

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -12,7 +12,7 @@
             <li><a href="histoire.php"><?php echo histoire?></a></li>
             <li><a href="prepvisite.php"><?php echo prepvisite?></a></li>
             <li><a href="login.php"><?php echo connect?></a></li>
-          <li><a href="inscription.php"><?php echo inscrire?></a></li>
+          <li><a href="register.php"><?php echo inscrire?></a></li>
           <li><a href="<?php echo $_SERVER["PHP_SELF"]; ?>?lang=fr">
           <img src="Images/français.png" height = "16px"
           width = "25px" alt="eng" align = "center" style = "margin: 0"/></a><li/>

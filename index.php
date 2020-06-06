48ad5de1ee627ab74d70da3b047a0c1e5f84b19b
JambonoLeJambon <60428074+JambonoLeJambon@users.noreply.github.com>
2020-06-06 18:08:26 +0200

    onglet histoire centrer + enlever partie droite

--- index.php
+++ index.php
@@ -7,17 +7,16 @@
         <div class="row">
             <!--Partie gauche du milieu de la page-->
             <div class="main">
-                <h1 style="text-align: center">Bienvenue à Versailles</h1>
+                <h1>Bienvenue à Versailles</h1>
                 <div class="slide">
                   <div class="image">
-
+                    <!--Slider-->
                   </div>
                 </div>
             </div>
             <!--Fin de la partie gauche du milieu de la page-->
             <!--Partie gauche du milieu de la page-->
             <div class="side">
-              <?php include_once('includes/billeterie.php') ?>
             </div>
             <!--Fin de la partie droite du milieu de la page-->
         </div>

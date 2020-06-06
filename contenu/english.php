6793cd9aa8dc3e7e7c4abdcd9d20e025aa07d9e0
Uniffay <56744682+Uniffay@users.noreply.github.com>
2020-06-06 15:53:01 +0200

    langage_suite

--- contenu/english.php
+++ contenu/english.php
@@ -25,4 +25,12 @@ define("developpeur", "Developer:");
 define("designer", "Designer:");
 
 // index
+
+define("bienvenue", "Welcome to Versailles");
+
+//chateau
+
+define("presentation", "Présentation Château");
+define("contenu1", "Inscrit depuis 1979 au patrimoine mondial de l'UNESCO, le château de Versailles fût construit par Louis XIII puis agrandit par son fils Louis XIV.")
+define("contenu2", "Les rois de France s'y sont succédé en embelissant le château chacun à leur tour jusqu'en 1789 lors de la révolution française.")
 ?>

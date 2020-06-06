0262f2d2c69023cd0aaeb2897771718e7048ed7c
Uniffay <56744682+Uniffay@users.noreply.github.com>
2020-06-06 15:41:42 +0200

    langue

---
+++ contenu/english.php
@@ -0,0 +1,28 @@
+<?php
+include("connexionBDD.php");
+define("langue", "Language");
+
+// header
+
+define("Titre_page", "Palace of Versailles");
+
+//navbar
+
+define("information", "Information");
+define("chateau", "Palace");
+define("visite", "Visit");
+
+//footer
+
+define("adresse", "Adress");
+define("tel", "Phone");
+define("appel_prix", "(call charge)");
+define("concepteur", "Website Creator :");
+define("chef", "Project Leader:");
+define("chercheur", "Researcher:");
+define("communiquant", "Communicator:");
+define("developpeur", "Developer:");
+define("designer", "Designer:");
+
+// index
+?>
---
+++ contenu/francais.php
@@ -0,0 +1,29 @@
+<?php
+//include("connexionBDD.php");
+define("langue", "Language");
+
+//header
+
+define("nom_chateau", "Château de Versailles");
+
+//navbar
+
+define("information", "Informations");
+define("chateau", "Château");
+define("visite", "Visite");
+
+//footer
+
+define("adresse", "Adresse");
+define("tel", "Tél:");
+define("appel_prix", "(prix d'un appel local)");
+define("concepteur", "Concepteur du site :");
+define("chef", "Chef de Projet:");
+define("chercheur", "Chercheur:");
+define("communiquant", "Communiquant:");
+define("developpeur", "Développeur:");
+define("designer", "Designer:");
+
+// index
+
+?>

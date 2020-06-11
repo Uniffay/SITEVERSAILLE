0f0c4a8d1669864b405d2a1b0a9ab81ca7a87806
VincentLeCodeur <vincent94420@icloud.com>
2020-06-11 15:59:31 +0200

    super slider !!!!!

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 6 seconds
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
 </script>
             </div>

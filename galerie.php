93bd3841b781eb30a2cf21d24170c577095de4fd
JambonoLeJambon <60428074+JambonoLeJambon@users.noreply.github.com>
2020-06-16 16:07:24 +0200

    Galerie flex

--- galerie.php
+++ galerie.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html>
   <?php include_once('includes/header.php') ?>
-  <link rel="stylesheet" href="quisommesnous.css" media="screen" type="text/css" />
+  <link rel="stylesheet" href="galerie.css" media="screen" type="text/css" />
   <body>
   <?php include_once('includes/navbar.php')?>
             <!--Partie gauche du milieu de la page-->
             <main>
-
+              <h1><?php echo Chateau_galerie ?></h1>
+              <div class="contenu_carou_auto">
+              <div class="caroussel-image">
+              <img src="Images/Versaille.jpg" alt>
+              <img src="Images/Versaille2.jpg" alt>
+              <img src="Images/Versaille3.jpg" alt>
+              <img src="Images/Chateau.jpg" alt>
+              <img src="Images/image.jpg" alt>
+              <img src="Images/Versaille4.jpg" alt>
+              <img src="Images/Versaille5.jpg" alt>
+              <img src="Images/Versaille6.jpg" alt>
+              </div>
+              </div>
             </main>
 
   <?php include_once('includes/footer.php') ?>

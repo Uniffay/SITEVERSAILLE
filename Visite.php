172b0bb803248f8faf069edc6ad1d2cb6e0a9ddc
Alan GARCIA <a.garcia.g007@gmail.com>
2020-06-16 18:45:00 +0200

    bug fix

--- Visite.php
+++ Visite.php
@@ -54,7 +54,6 @@
       				<br>
       					> <?php echo siecle3_visite?>
     				</p>
-            <img src=" "/>
           </div>
           <!--Fin de la partie gauche du milieu de la page-->
         </div>

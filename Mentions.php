ccc4fc7d451ac3eccff87da9db9239e52894a5ca
Khabuzy <60428132+Khabuzy@users.noreply.github.com>
2020-06-16 18:31:28 +0200

    Mentions légales

--- Mentions.php
+++ Mentions.php
@@ -5,26 +5,59 @@
         <div class="row">
             <!--Partie gauche du milieu de la page-->
             <div class="main">
-            <h1><?php echo mentionslegales ?></h1>
-				<p>
-				<?php echo contenu1_mention ?>
-				<?php echo contenu2_mention ?>
-				<?php echo contenu3_mention ?>
-				<br>
-        <br>
-        </p>
-            <h1><?php echo editeur_site ?> </h1>
-        <p>
-        <?php echo contenu4_mention ?>
-        <br>
-        GANDEGA Issa, GUEFFIER Benjamin, FRANCISCO Michel, BOULET Quentin, GARCIA CALZADA Alan, DOMINGUES Vincent.
-        <br>
-        <br>
-        </p>
-          <h1><?php echo conditions_duti ?> </h1>
-        <p>
-        <?php echo contenu5_mention ?>
-
+              <h1><?php echo mentionslegales ?></h1>
+      				<p>
+      				<?php echo contenu1_mention ?>
+      				<?php echo contenu2_mention ?>
+      				<?php echo contenu3_mention ?>
+      				<br>
+              <br>
+              </p>
+                  <h2><?php echo editeur_site ?> </h2>
+              <p>
+              <b><?php echo contenu4_mention ?></b>
+              <br>
+              GANDEGA Issa, GUEFFIER Benjamin, FRANCISCO Michel, BOULET Quentin, GARCIA CALZADA Alan, DOMINGUES Vincent.
+              <br>
+              <br>
+              </p>
+                <h2><?php echo conditions_duti ?> </h2>
+              <p>
+              <?php echo contenu5_mention ?>
+              <br>
+              <br>
+              </p>
+                <h2><?php echo limi_resp ?> </h2>
+              <p>
+                <?php echo contenu6_mention ?>
+                <?php echo contenu7_mention ?>
+              <br>
+              <br>
+              </p>
+                <h2><?php echo privacy ?> </h2>
+              <p>
+                <?php echo contenu8_mention ?>
+              <br>
+              <br>
+              </p>
+                <h2><?php echo propri_inte ?> </h2>
+              <p>
+              <?php echo contenu9_mention ?>
+              <?php echo contenu10_mention ?>
+              <?php echo contenu11_mention ?>
+              <br>
+              <br>
+              </p>
+                <h2><?php echo cookies ?> </h2>
+              <p>
+              <?php echo contenu12_mention ?>
+              <br>
+              <br>
+              </p>
+                <h2><?php echo contact ?> </h2>
+              <p>
+              <?php echo contenu13_mention ?>
+              </p>
             </div>
             <!--Fin de la partie gauche du milieu de la page-->
         </div>
